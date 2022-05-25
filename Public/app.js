2df682ad6c2051373d1b2a55f45c1e13b7b26c9b
Michael Howard <mike.gene.howard@gmail.com>
2022-05-24 15:07:25 -1000

    updated app.js

--- Public/app.js
+++ Public/app.js
@@ -5,7 +5,7 @@ const $viewAll = $('#displayAll_btn');
 const $viewOne = $('#displayOne_btn');
 
 // Event Listeners
-$addEntry.on('click', () => {
+$viewAll.on('click', () => {
     let date = $('.tracker__date').val();
     let type = $('.tracker__type').val();
     let muscleGroup = $('.tracker__muscle-group').val();
@@ -32,30 +32,39 @@ $addEntry.on('click', () => {
             var $type = $('<li>').addClass('type').text("Type: " + `${data[i].type}`);
             $type.appendTo($ul);
 
+            // Add muscle group info, append to ul
             if (data[i].muscle_group !== null){
                 var $muscleGroup = $('<li>').addClass('muscle-group').text("Muscle Group: " + `${data[i].muscle_group}`);
                 $muscleGroup.appendTo($ul);
-              }
-    
-            // if (show.genres !== null){
-            //   var $h2 = $('<h2></h2>').addClass('card-generes').text(`${show.genres.join(', ')}`);
-            //   $h2.appendTo($resultCard);
-            // }
-      
-            // if (show.summary !== null){
-            //   var $summaryDiv = $('<div></div>').addClass('card-summary')
-            //   $('<em></em>').text('Summary:').appendTo($summaryDiv);
-      
-            //   var summaryText = show.summary;
-            //   $(summaryText).appendTo($summaryDiv);
-      
-            //   $summaryDiv.appendTo($resultCard);
-            // }
-            
-            // if (show.url !== null){
-            //   var $link = $('<a>Visit Show</a>').attr('href', show.url);
-            //   $link.appendTo($resultCard);
-            // }
+            }
+
+            // Add exercise info, append to ul
+            var $exercise = $('<li>').addClass('exercise').text("Exercise: " + `${data[i].exercise_name}`);
+            $exercise.appendTo($ul);
+
+            // Add sets info, if applicable, append to ul
+            if (data[i].sets !== null){
+                var $sets = $('<li>').addClass('sets').text("Sets: " + `${data[i].sets}`);
+                $sets.appendTo($ul);
+            }
+
+            // Add reps info, if applicable, append to ul
+            if (data[i].reps !== null){
+                var $reps = $('<li>').addClass('reps').text("Reps: " + `${data[i].reps}`);
+                $reps.appendTo($ul);
+            }
+
+            // Add strength conditioning weight info, if applicable, append to ul
+            if (data[i].totat_weight !== null){
+                var $weight = $('<li>').addClass('weight').text("Heaviest Weight: " + `${data[i].total_weight}`);
+                $weight.appendTo($ul);
+            }
+
+            // Add workout duration info, if applicable, append to ul
+            if (data[i].duration !== null){
+                var $duration = $('<li>').addClass('duration').text("Duration/Time: " + `${data[i].duration}`);
+                $duration.appendTo($ul);
+            }
       
             $resultCard.appendTo('.results');
           }
