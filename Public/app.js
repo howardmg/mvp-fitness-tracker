0c0735cad8325d8558e49e33dc92690f31886ffb
Michael Howard <mike.gene.howard@gmail.com>
2022-05-24 14:31:33 -1000

    updated app.js and added results div to index.html

--- Public/app.js
+++ Public/app.js
@@ -16,13 +16,47 @@ $addEntry.on('click', () => {
     let duration = $('.tracker__duration'). val();
 
     $.get('https://warm-basin-09430.herokuapp.com/test', (data) => {
-        console.log(data)
+        console.log(data);
+
+        for (var i = 0; i < data.length; i++){
+            var $resultCard = $('<div></div>').addClass('result-card')
+
+            var $ul = $('<ul></ul>').addClass('workout')
+            $ul.appendTo($resultCard)
+      
+            if (data[i].date !== null){
+              var $li = $('<li>').addClass('date').text(`${data[i].date}`);
+              $li.appendTo($ul);
+            }
+      
+            // if (show.genres !== null){
+            //   var $h2 = $('<h2></h2>').addClass('card-generes').text(`${show.genres.join(', ')}`);
+            //   $h2.appendTo($resultCard);
+            // }
+      
+            // if (show.summary !== null){
+            //   var $summaryDiv = $('<div></div>').addClass('card-summary')
+            //   $('<em></em>').text('Summary:').appendTo($summaryDiv);
+      
+            //   var summaryText = show.summary;
+            //   $(summaryText).appendTo($summaryDiv);
+      
+            //   $summaryDiv.appendTo($resultCard);
+            // }
+            
+            // if (show.url !== null){
+            //   var $link = $('<a>Visit Show</a>').attr('href', show.url);
+            //   $link.appendTo($resultCard);
+            // }
+      
+            $resultCard.appendTo('.results');
+          }
     })
 })
 
-$viewAll.on('click', () => {
-    let workoutDate = $('.tracker__date').val();
-    console.log(workoutDate)
-})
+// $viewAll.on('click', () => {
+//     let workoutDate = $('.tracker__date').val();
+//     console.log(workoutDate)
+// })
 
 // Return and display a day's workout
